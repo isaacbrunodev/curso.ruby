91bd5ca7083c596b3989b6224f031534a01c441c
Isaac Bruno <112228099+isaacbrunodev@users.noreply.github.com>
2023-07-22 23:47:36 -0300

    Update faker.rb
    conclusão do código

--- faker.rb
+++ faker.rb
@@ -13,4 +13,22 @@ class Usuario
       "Nome: #{@nome}, Email:#{@email}, Telefone: #{@Telefone}}"
     end
 end
+def to_s
+    "Nome:#{@nome}, Email:#{@email}, Telefone: #{@telefone}"
+    end
+end
+
+#Criação de uma coleção de usuários usando a gem "faker"
+usuarios=[]
+10.time do
+    nome=Faker:: Name.Name
+    email=Faker::Internet.email
+    telefone=Faker::PhoneNumber.phone_number
+    usuarios<< Usuario.new(nome,email,telefone)
+end
+
+#Imprime os usuários criados
+usuarios.each do |usuario|
+    puts usuario.to_s
+end     
 
