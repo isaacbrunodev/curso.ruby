d7fc48b171e47849a51e9d2d8e8932c50c2281e6
Isaac Bruno <112228099+isaacbrunodev@users.noreply.github.com>
2023-07-24 22:41:49 -0300

    Update faker.rb
    realizadas correções no código

--- faker.rb
+++ faker.rb
@@ -1,34 +1,30 @@
 require 'faker'
-#Classe para representar um usuário
+
+# Classe para representar um usuário
 class Usuario
-    attr_acessor :nome,:email,:telefone
+  attr_accessor :nome, :email, :telefone
 
-    def initialize(nome,email,telefone)
-        @nome=nome
-        @email=email
-        @telefone=telefone
-    end
-    
-    def to_s
-      "Nome: #{@nome}, Email:#{@email}, Telefone: #{@Telefone}}"
-    end
-end
-def to_s
-    "Nome:#{@nome}, Email:#{@email}, Telefone: #{@telefone}"
-    end
+  def initialize(nome, email, telefone)
+    @nome = nome
+    @email = email
+    @telefone = telefone
+  end
+
+  def to_s
+    "Nome: #{@nome}, Email: #{@email}, Telefone: #{@telefone}"
+  end
 end
 
-#Criação de uma coleção de usuários usando a gem "faker"
-usuarios=[]
-10.time do
-    nome=Faker:: Name.Name
-    email=Faker::Internet.email
-    telefone=Faker::PhoneNumber.phone_number
-    usuarios<< Usuario.new(nome,email,telefone)
+# Criação de uma coleção de usuários usando a gem "faker"
+usuarios = []
+10.times do
+  nome = Faker::Name.name
+  email = Faker::Internet.email
+  telefone = Faker::PhoneNumber.phone_number
+  usuarios << Usuario.new(nome, email, telefone)
 end
 
-#Imprime os usuários criados
+# Imprime os usuários criados
 usuarios.each do |usuario|
-    puts usuario.to_s
-end     
-
+  puts usuario.to_s
+end
