ac7ca6699a014942bd72a8597353ba413b39600d
Isaac Bruno <isaacbruno1996@gmail.com>
2023-08-03 23:16:07 -0300

    alteracao block

--- AULA-8/block.rb
+++ AULA-8/block.rb
@@ -1,8 +1,15 @@
-foo ={2=> 3,4=>5}
-foo.each do |key,value|
-  puts "key=#{key}"
-  puts "value=#{value}"
-  puts"key*value=#{key*value}"
-  puts '---'
-end
- 
+def foo(numbers,&block)
+    if block_given?
+        numbers.each do |key,value|
+            block.cal(key,value)
+        end
+    end
+ end          
+
+ numbers = {1=>2.3+>3,4+>4}
+
+ foo{numbers} do |key,value|
+    puts "#{key} * #{value} = #{key * value}"
+    puts"#{key}+#{value}=#{key+value}"
+    puts"---"
+ end   
