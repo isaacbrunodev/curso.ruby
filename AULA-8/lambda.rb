f37d159f67b83ef81f0f5bfbda85f3848de60fa8
Isaac Bruno <isaacbruno1996@gmail.com>
2023-08-04 23:02:58 -0300

    lambda com bloco

--- AULA-8/lambda.rb
+++ AULA-8/lambda.rb
@@ -1,3 +1,14 @@
-first_lambda= -> (name){names.each { |name| puts name} }
-names={"João","Maria","Pedro" }
-first_lambda.call(names)
+my_lambda= lambda do |numbers|
+index=0
+puts 'úmero atual+Próximo número'
+numbers.each do |number|
+    return if numers[index]==numbers.last
+    puts "(#{numbers[index]})+(#{numbers[index + 1]})"
+    puts numbers[index] + numbers[index+1]
+    index
+  end
+end
+
+numbers = [1,2,3,4]
+
+my_lambda.call(numbers) 
