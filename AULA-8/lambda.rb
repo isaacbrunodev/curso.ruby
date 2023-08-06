c605b3621262fcf14ef5943c782c083bc6c8f061
Isaac Bruno <isaacbruno1996@gmail.com>
2023-08-05 21:38:32 -0300

    correcao lambda

--- AULA-8/lambda.rb
+++ AULA-8/lambda.rb
@@ -1,8 +1,8 @@
 my_lambda= lambda do |numbers|
 index=0
-puts 'úmero atual+Próximo número'
+puts 'número atual+Próximo número'
 numbers.each do |number|
-    return if numers[index]==numbers.last
+    return if numbers[index]==numbers.last
     puts "(#{numbers[index]})+(#{numbers[index + 1]})"
     puts numbers[index] + numbers[index+1]
     index
