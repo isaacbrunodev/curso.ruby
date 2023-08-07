f4ef2f94a5bf93c8e9f3d3b035ca1646460dc533
Isaac Bruno <isaacbruno1996@gmail.com>
2023-08-07 13:31:12 -0300

    mixins criando objeto

--- AULA-8/mixins.rb
+++ AULA-8/mixins.rb
@@ -6,4 +6,26 @@ module ImpressaoDecorada
       puts decoracao
   end
 end
-    
+
+module Pernas
+include ImpressaoDecorada
+
+def chute_frontal
+    imprimir 'Chute frontal'
+end
+
+def chute_lateral
+   imrpimir 'Chute lateral'
+end
+
+module Bracos
+include ImpressaoDecorada
+
+def jab_de_direita
+    imprimir 'Jab de esquerda'
+end
+
+def gancho
+    imprimir 'Gancho'
+  end
+end      
