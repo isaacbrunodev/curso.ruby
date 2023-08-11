d281948e7009897ad44ec58ac4394217e9d2296d
Isaac Bruno <isaacbruno1996@gmail.com>
2023-08-10 23:51:48 -0300

    acréscimo lamdba

--- lambda2.rb
+++ lambda2.rb
@@ -1,6 +1,11 @@
 #Criando uma lista de números
 numeros=[1,2,3,4,5,6,7,8,9,19]
 
+#Criando uma lambda para calcular a média
+calcular_media=lambda do |lista|
+total=lista.reduce(:+)
+otal.to_f /lista.length
+
 #Criando uma lambda para filtrar números pares
 filtrar_pares=lambda { |num| num.even?}
 
@@ -15,7 +20,7 @@ quadrados=numeros.map(&filtrar_pares)
 
 #Elevando números ao quadrado usando a lambda
 quadrados =numeros.map(&elevar_ao_quadrado)
-
+  
 #Imprimindo os resultados
 puts "Números pares:#{pares}"
 puts "Quadrados dos números: #{quadrados}"
