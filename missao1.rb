abeb684e6e65f0afb5048fd3ef3010b69eeb7195
Isaac Bruno <isaacbruno1996@gmail.com>
2023-08-11 01:23:59 -0300

    correção bug missao 1

--- missao1.rb
+++ missao1.rb
@@ -1,7 +1,5 @@
 #Criação da lambda
 primeira_letra_maiuscula = lambda { |nome| puts nome.capitalize}
-   puts nome.capitalize
-end
 
 # Método que chama a lambda
 def capitalize_name(lambda_capitalize)
