79e8f4ae014d3948149da2c01126b613aaf2528f
Isaac Bruno <112228099+isaacbrunodev@users.noreply.github.com>
2023-08-18 22:54:18 -0300

    Update protected.rb
    update

--- AULA-9/protected.rb
+++ AULA-9/protected.rb
@@ -1,16 +1,29 @@
-class Foo
-  def call_protected(instance)
-  instance.bar
+class Person
+  def initialize(name,age)
+    @name= name
+    @age=age
   end
   
-  protected  
+  def introduce(other_person)
+    puts"olá,#{other_person.name}! Meu nome é #{@name}."
+  end
 
-  def bar
-   puts "protected method"
+  protected
+
+  def name
+    @name
   end
 end
 
-instance_1 =Foo.new
-instance_2=Foo.new
-instance_1.call_private(instance_1)
-instance_1.call_private(instance_2)
+class Friend<Person 
+   def greet(other_person)
+     puts "Oi, #{other_person.name}! Como vai? "
+   end
+  end
+  
+  person1=PErson.new("Alice",30)
+  person2=Person.new("Bob",25)
+  friend=Friend.new("Charlie",28)
+
+  person1.introduce(person2) 
+  friend.greet(person1)
